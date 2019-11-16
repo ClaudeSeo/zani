706d2de330182a9bed16593baef0ee0062d6cf0d
ClaudeSeo <ehdaudtj@gmail.com>
2019-11-17 03:39:02 +0900

    fix: UUID 파싱 버그 수정

--- src/component/utils.ts
+++ src/component/utils.ts
@@ -2,5 +2,5 @@ import { v4 } from 'uuid';
 
 export const makeSecret = (): string => {
     const uuid = v4().split('-');
-    return `${uuid[3]}${uuid[2]}${uuid[1]}${uuid[5]}${uuid[4]}`;
+    return `${uuid[2]}${uuid[1]}${uuid[0]}${uuid[4]}${uuid[3]}`;
 };
